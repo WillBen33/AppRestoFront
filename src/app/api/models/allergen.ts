55b2c5ffc21ec9a3073137293299ddbdbe885860
Will Ben <wilfrid.benoit@outlook.fr>
2021-12-28 18:09:56 +0100

    Commit after update gitignore

--- src/app/api/models/allergen.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-import { Product } from './product';
-export interface Allergen {
-  id?: number;
-  libelle: string;
-  products?: Array<Product>;
-}
--- src/app/api/models/role.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-import { Profil } from './profil';
-export interface Role {
-  id?: number;
-  libelle: string;
-  profils: Array<Profil>;
-}
