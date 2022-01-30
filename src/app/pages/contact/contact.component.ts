ac777471e24e461a2ec9920ce79c68aa6543181e
Will Ben <wilfrid.benoit@outlook.fr>
2022-01-30 16:10:48 +0100

    modification clé google recaptcha

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
@@ -7,10 +9,23 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  token: string|undefined;
+
   constructor() {
+    this.token = undefined;
   }
 
-  ngOnInit(): void {
+  public send(form: NgForm): void {
+    if (form.invalid) {
+      for (const control of Object.keys(form.controls)) {
+        form.controls[control].markAsTouched();
+      }
+      return;
+    }
+
+    console.debug(`Token [${this.token}] generated`);
   }
+
+  ngOnInit(): void {}
 
 }
